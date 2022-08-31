adcbf211d99eb903ced65c718f75a806298c83b8
adityapryg <aditya@brainmatics.com>
2022-08-31 20:39:59 +0700

    [ADD] Child komponen dan style dari list user, UserItem

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './UserItem.css';
+
+const UserItem = props => {
+  return (
+    <li className="user-item">
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <img src={props.image} alt={props.name} />
+        </div>
+        <div className="user-item__info">
+          <h2>{props.name}</h2>
+          <h3>
+            {props.todoCount} {props.todoCount === 1 ? 'Todo' : 'Todos'}
+          </h3>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default UserItem;
