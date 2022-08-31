3e2efe7b0e557629a226e6a1e9b32b7895499bd4
adityapryg <aditya@brainmatics.com>
2022-09-01 01:27:00 +0700

    [ADD] React useReducer

--- src/shared/FormElements/Input.js
+++ src/shared/FormElements/Input.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import './Input.css';
 
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case 'ONCHANGE':
+      return {
+        ...state,
+        value: action.val,
+        isValid: true
+      };
+    default:
+      return state;
+  }
+}
+
 const Input = props => {
+
+  // const [inputValue, setInputValue] = useState('');
+  // const [isValid, setIsValid] = useState('');
+  /**
+   * useReducer for more complex managing state
+   */
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: '',
+    isValid: false
+  })
+
+  const inputChangeHandler = event => {
+    //handleinput
+  }
+
   const element =
     props.element === 'input' ? (
-      <input id={props.id} type={props.type} placeholder={props.placeholder} />
+      <input 
+        id={props.id} 
+        type={props.type} 
+        placeholder={props.placeholder} 
+        onChange={inputChangeHandler}
+      />
     ) : (
-      <textarea id={props.id} rows={props.rows || 3} />
+      <textarea 
+        id={props.id} 
+        rows={props.rows || 3} 
+      />
     );
 
   return (
