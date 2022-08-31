7d94131701f6a407975b00df57088a81f17062b2
adityapryg <aditya@brainmatics.com>
2022-08-31 20:28:35 +0700

    [ADD] Menggunakan Router pada app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React from 'react';
+import {
+  /**
+   * Sebagai router yang menggunakan API history dari HTML5
+   * sehingga dapat menggunakan location untuk mensingkronasi UI dengan url
+   */
+  BrowserRouter as Router, 
+  Route,
+} from 'react-router-dom';
 import './App.css';
+import NewTodo from './todo/pages/NewTodo';
+import Users from './user/pages/Users';
+
 
 function App() {
-  return <h1>Let&#39;s the game begin!</h1>;
+  return (
+      <Router>
+        <Route path="/">
+          <Users/>
+        </Route>
+        <Route path="/todo/new">
+          <NewTodo/>
+        </Route>
+      </Router>
+  );
 }
 
 export default App;
