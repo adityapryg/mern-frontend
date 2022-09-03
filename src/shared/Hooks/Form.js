3662f670f4dfdaf328ee533403be4c98faa4050b
adityapryg <aditya@brainmatics.com>
2022-09-03 08:49:28 +0700

    [RFR] Refactor reducer to custom hooks

---
+++ src/shared/Hooks/Form.js
@@ -0,0 +1,43 @@
+import { useReducer, useCallback } from "react";
+
+const formReducer = (state, action) => {
+    switch (action.type) {
+      case 'INPUT_CHANGE':
+        let formIsValid = true;
+        for (const inputId in state.inputs) {
+          if (inputId === action.inputId) {
+            formIsValid = formIsValid && action.isValid;
+          } else {
+            formIsValid = formIsValid && state.inputs[inputId].isValid;
+          }
+        }
+        return {
+          ...state,
+          inputs: {
+            ...state.inputs,
+            [action.inputId]: { value: action.value, isValid: action.isValid }
+          },
+          isValid: formIsValid
+        };
+      default:
+        return state;
+    }
+  };
+
+export const useForm = (formInputs, isFormValid) => {
+    const [formState, dispatch] = useReducer(formReducer, {
+        inputs: formInputs,
+        isValid: isFormValid
+      });
+
+      const inputHandler = useCallback((id, value, isValid) => {
+        dispatch({
+          type: 'INPUT_CHANGE',
+          value: value,
+          isValid: isValid,
+          inputId: id
+        });
+      }, []);
+
+      return [formState, inputHandler];
+}
