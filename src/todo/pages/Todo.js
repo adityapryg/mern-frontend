0ce8e3d0feb41e2c074277d03af79e3c9d8a0841
adityapryg <aditya@brainmatics.com>
2022-09-04 02:36:50 +0700

    [WIP] Fetch all todo

--- src/todo/pages/Todo.js
+++ src/todo/pages/Todo.js
@@ -1,36 +1,37 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
 import Button from "../../shared/FormElements/Button";
 import TodoList from "../components/TodoList";
 
 import './Todo.css';
 
-const DUMMY_TODO = [
-  {
-    id: 'tid1',
-    title: 'Belajar React',
-    description: 'Membangun interface aplikasi todo list',
-    creator: 'uid1'
-  },
-  {
-    id: 'tid2',
-    title: 'Belajar Express Node',
-    description: 'Membangun REST API',
-    creator: 'uid2'
-  }
-]
-
 const Todo = () => {
+    const [loadedTodos, setLoadedTodo] = useState();
 
-    let userID = useParams().userID;
-    let requestedTodo = [...DUMMY_TODO];
+    // let userID = useParams().userID;
+    // let requestedTodo = [...DUMMY_TODO];
+
+    useEffect(() => {
+      const sendRequest = async () => {
+        const response = await fetch('http://localhost:5000/api/todo');
+        const responseData = await response.json();
+
+        //if not 200'ish response code
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setLoadedTodo(responseData.todos)
+      }
+      sendRequest();
+    }, []);
+
     return (
       <section>
         <div className="todo">
           <Button to="/todo/new">Create</Button>
         </div>
         <TodoList 
-          items={ userID ? requestedTodo.filter(todo => todo.creator === userID) : DUMMY_TODO} 
+          items={ loadedTodos } 
         />
       </section>
     )
